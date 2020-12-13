261bd68a89d7a5bab96ca77a067d9b440897dec1
Moldavskiy Denis <damoldavskiy@edu.hse.ru>
2020-12-13 21:37:20 +0300

    TypeList

--- module-1/homework/TypeList/typelist/append.h
+++ module-1/homework/TypeList/typelist/append.h
@@ -2,5 +2,29 @@
 
 #include "typelist.h"
 
-template<typename TList, typename NewType>
+template <typename TList, typename NewType>
 struct Append;
+
+template <>
+struct Append<NullType, NullType>
+{
+    typedef NullType NewTypeList;
+};
+
+template <typename T>
+struct Append<NullType, T>
+{
+    typedef TypeList<T, NullType> NewTypeList;
+};
+
+template <typename Head, typename Tail>
+struct Append<NullType, TypeList<Head, Tail>>
+{
+    typedef TypeList<Head, Tail> Result;
+};
+
+template <typename Head, typename Tail, typename T>
+struct Append<TypeList<Head, Tail>, T>
+{
+    typedef TypeList<Head, typename Append<Tail, T>::NewTypeList> NewTypeList;
+};
--- module-1/homework/TypeList/typelist/replace.h
+++ module-1/homework/TypeList/typelist/replace.h
@@ -2,5 +2,23 @@
 
 #include "typelist.h"
 
-template<typename TList, typename OldType, typename NewType> 
+template<typename TList, typename OldType, typename NewType>
 struct Replace;
+
+template <typename T, typename U>
+struct Replace<NullType, T, U>
+{
+    typedef NullType NewTypeList;
+};
+
+template <typename T, typename Tail, typename U>
+struct Replace<TypeList<T, Tail>, T, U>
+{
+    typedef TypeList<U, Tail> NewTypeList;
+};
+
+template  <typename Head, typename Tail, typename T, typename U>
+struct Replace<TypeList<Head, Tail>, T, U>
+{
+    typedef TypeList<Head, typename Replace<Tail, T, U>::NewTypeList> NewTypeList;
+};
